632af03fbdde0d9735dd68435ce807c7d468e471
unknown <superay1110@outlook.com>
2019-03-01 10:14:17 -0600

    Data loading

---
+++ Loading data.R
@@ -0,0 +1,6 @@
+years <- c(1991,1993,1996,1999,2002,2005,2008,2011,2014,2017)
+dta <- list()
+for (i in 1:10) {
+  name <- str_c("DATA/NYCHVS ", years[i]," Occupied File for ASA Challenge.csv")
+  dta[[i]] <- read_csv(name, skip = 1)
+}
