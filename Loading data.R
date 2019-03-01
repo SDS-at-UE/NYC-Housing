4397d132b095315847b475a9ceb2f830579723fe
Albin <ca133@evansville.edu>
2019-03-01 10:30:06 -0600

    Combining rows in tables

--- Loading data.R
+++ Loading data.R
@@ -4,3 +4,10 @@ for (i in 1:10) {
   name <- str_c("DATA/NYCHVS ", years[i]," Occupied File for ASA Challenge.csv")
   dta[[i]] <- read_csv(name, skip = 1)
 }
+
+NYC <- dta[[1]] %>% select(`Reason for Moving`, `Mortgage Status`, `Year Identifier`) 
+for (i in 2:10) {
+  dta[[i]] %>% select(`Reason for Moving`, `Mortgage Status`, `Year Identifier`) %>%
+    bind_rows(NYC) -> 
+    NYC
+}
