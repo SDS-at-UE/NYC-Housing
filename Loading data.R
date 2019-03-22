f8f09e9dcf9272e601986fdabbe013364e692483
Albin <ca133@evansville.edu>
2019-03-22 10:37:22 -0500

    New loading data method

--- Loading data.R
+++ Loading data.R
@@ -11,3 +11,28 @@ for (i in 2:10) {
     bind_rows(NYC) -> 
     NYC
 }
+
+
+
+# New Method
+years <- c(1991,1993,1996,1999,2002,2005,2008,2011,2014,2017)
+dta <- list()
+for (i in 1:10) {
+  name <- str_c("DATA/NYCHVS ", years[i]," Occupied File for ASA Challenge.csv")
+  dta[[i]] <- read_csv(name, skip = 1)
+}
+
+NYC <- dta[[1]]
+for (i in 2:3) {
+  dta[[i]] %>% 
+    bind_rows(NYC) -> 
+    NYC
+}
+for (i in 4:9) {
+  dta[[i]] %>% dplyr::select(-`Floor of unit`) %>%
+    bind_rows(NYC) -> 
+    NYC
+}
+dta[[10]] %>% 
+  bind_rows(NYC) -> 
+  NYC
