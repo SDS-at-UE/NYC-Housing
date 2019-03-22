f9b67e53c93a6b8ddcfe2424b9c3c868f6ecb398
Mozina <mm696@evansville.edu>
2019-03-22 10:49:01 -0500

    Continued Experimentation

--- Experimentation 2.R
+++ Experimentation 2.R
@@ -9,8 +9,6 @@ for (i in 1:10) {
   dta[[i]] <- read_csv(name, skip = 1)
 }
 
-
-
 NYC <- dta[[1]] 
 for (i in 2:10) {
   dta[[i]] %>% 
@@ -24,16 +22,29 @@ NYC <- NYC %>% mutate(Borough = ifelse(Borough == 3, "Manhattan", Borough))
 NYC <- NYC %>% mutate(Borough = ifelse(Borough == 4, "Queens", Borough))
 NYC <- NYC %>% mutate(Borough = ifelse(Borough == 5, "Staten Island", Borough))
 
-newdataooprent <- NYC %>% select(`Out of pocket rent`,`Year Identifier`) %>% 
-  filter(!(`Out of pocket rent` %in% c(99998, 99999)), 
-         `Out of pocket rent` < 6000) %>% 
+## Need to combine the columns from 91-99 and 02-17
+NYC %>% mutate(waterleakage = ifelse(`Year Identifier` > 2000, 
+                                     `Water leakage inside apartment`,
+                                     `Water leakage inside apartment (house)`)) %>%
+  select(waterleakage, `Year Identifier`) %>% View()
+
+newdatarent <- NYC %>% select(`Monthly contract rent`,`Year Identifier`) %>% 
+  filter(!(`Monthly contract rent` %in% c(99998, 99999))) %>% 
+  na.exclude()
+
+newdataooprent <- NYC %>% select(`Out of pocket rent`, `Year Identifier`) %>%
+  filter(!(`Out of pocket rent` %in% c(99998, 99999))) %>%
   na.exclude()
 
 newdatavalue <- NYC %>% select(Value,`Year Identifier`) %>%
   filter(!(Value %in% c(9999998, 9999999)), Value < 3990000) %>% na.exclude()
 
+newdatarent %>% group_by(`Year Identifier`) %>% count()
+newdataooprent %>% group_by(`Year Identifier`) %>% count()
+newdatavalue %>% group_by(`Year Identifier`) %>% count()
 
 
+### Divide data into own and rent
+own <- NYC %>% filter(`Tenure 1` == 1)
+rent <- NYC %>% filter(`Tenure 1` == 9)
 
-
-                                                       
