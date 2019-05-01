bd7a8c74b023688173c90631bb21bb0ee1420357
Supperay <superay1110@outlook.com>
2019-05-01 10:45:09 -0500

    nyc mapping

--- NYCmapping.R
+++ NYCmapping.R
@@ -11,14 +11,16 @@ plot(nyc)
 nyc <- nyc[,c(2,3,35)]
 zscore$"Borough.and.Sub.Borough.Area" -> zscore$code
 nyc <- right_join(nyc, zscore)
+name <- nyc$name
 
 # Bronx
 nyc %>% filter(`Borough and Sub-Borough Area` < 200) %>% tm_shape() +
   tm_fill("Average",title="Quality Index", palette="BuGn")  +
   tm_borders() +
   tm_text("code") +
-  tm_layout(title = "Bronx", title.position = c("right","bottom")) +
-  tm_add_legend(type = "symbol", labels = c("a","b","c","d","e","f","g","h","i","j"))
+  tm_add_legend(type = "text", labels = name[c(1:10)], text = c(101,102,103,104,105,106,107,108,109,110)) +
+  tm_layout(title = "Bronx",legend.outside = TRUE, legend.text.size = 1) 
+
 
 # Brooklyn
 nyc %>% filter(`Borough and Sub-Borough Area` < 300 & `Borough and Sub-Borough Area` > 200) %>% tm_shape() +
